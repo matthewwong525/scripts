f8295ba07d6ad87c237af7d439a69f419cdaecd4
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2021-05-12 13:59:23 -0400

    Add random person to talk to in my wp_template

--- wp_template.py
+++ wp_template.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from datetime import timedelta as td
+import random
 
 
 def suffix(d):
@@ -14,28 +14,67 @@ today = dt.date.today()
 mon = today + dt.timedelta(days=-today.weekday(), weeks=1)
 week_list = []
 for i in range(8):
-    week_list.append(custom_strftime('%B {S}, %Y', mon + dt.timedelta(days=i)))
+    t = mon + dt.timedelta(days=i)
+    week_list.append(t.strftime('%Y-%m-%d'))
 
 mon, tues, wed, thurs, fri, sat, sun, next_mon = week_list
-query = """- ## Top Weekly Goals
-    - Misc
-        - {{{{[[TODO]]}}}} Create [[Weekly Plan: {next_mon}]]
-        - {{{{[[TODO]]}}}} Clear 5 items from my inbox
-- ## Daily Goals
-    - Monday [[{mon}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{mon}]]{{not: [[query]]}}}} }}}}
-    - Tuesday [[{tues}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{tues}]]{{not: [[query]]}}}} }}}}
-    - Wednesday [[{wed}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{wed}]]{{not: [[query]]}}}} }}}}
-    - Thursday [[{thurs}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{thurs}]]{{not: [[query]]}}}} }}}}
-    - Friday [[{fri}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{fri}]]{{not: [[query]]}}}} }}}}
-    - Saturday [[{sat}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{sat}]]{{not: [[query]]}}}} }}}}
-    - Sunday [[{sun}]]
-        - {{{{[[query]]: {{and: {{or: [[TODO]] [[DONE]]}} [[{sun}]]{{not: [[query]]}}}} }}}}
-- Tags:: #[[Weekly Plan]]""".format(mon=mon, tues=tues, wed=wed, thurs=thurs, fri=fri, sat=sat, sun=sun, next_mon=next_mon)
+msg_ppl = [
+'Maddie Lee',
+'Bernard Tung',
+'Katie Li',
+'Kenta Morris',
+'Janakan Chandrakumar',
+'Marco Gao',
+'Ivan Leung',
+'Ethan Liong',
+'Eric Fung',
+'Jason Ho-Shue',
+'Kevin Widjaja',
+'Henry Ngan',
+'Philip So',
+'Elvis Ho',
+'James Allen Yu',
+'Arjun Nair',
+'Jessica Wong',
+'Jonah Yang',
+'Bryan Widjaja',
+'Nikki Wong' ]
+rand_pers = random.choice(msg_ppl)
+
+query = """tags: #WeeklyPlan
+## Top Weekly Goals
+### Misc
+- [ ] Create [[Weekly Plan - {next_mon}]] [[{sun}]]
+- [ ] Clear 5 items from my inbox [[{fri}]]
+- [ ] Message / Hangout with this person: [[{rand_pers}]]
+## Daily Goals
+### Monday [[{mon}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{mon}]] -"line:(")
+```
+### Tuesday [[{tues}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{tues}]] -"line:(")
+```
+### Wednesday [[{wed}]]
+```query
+line:(("- [ ]" OR "- [x]")[[{wed}]] -"line:(")
+```
+### Thursday [[{thurs}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{thurs}]] -"line:(")
+```
+### Friday [[{fri}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{fri}]] -"line:(")
+```
+### Saturday [[{sat}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{sat}]] -"line:(")
+```
+### Sunday [[{sun}]]
+```query
+line:(("- [ ]" OR "- [x]") [[{sun}]] -"line:(")
+```""".format(mon=mon, tues=tues, wed=wed, thurs=thurs, fri=fri, sat=sat, sun=sun, next_mon=next_mon, rand_pers=rand_pers)
 
 print(query)
